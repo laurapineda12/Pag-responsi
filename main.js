c6d1e99a14aa1d3c8665b67b200afe08d7dd2797
Aprendiz <Aprendiz@SGLDFPCAT7F567.FormacionSena.RED>
2018-08-03 12:25:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn-bar')
+const nav = document.querySelector('.nav')
+
+btn.addEventListener('click', () => {
+  console.log(nav)
+  nav.classList.toggle('slide-nav')
+})
